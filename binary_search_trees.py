c43b2b63da783f4d8cb10ed5b86112f508e80f32
Amel <amel.ntamark.ic@smartsheet.com>
2022-10-01 13:38:58 -0700

    add methods to binary search tree

--- binary_search_trees.py
+++ binary_search_trees.py
@@ -11,19 +11,64 @@ class TreeNode:
 class Tree:
     def __init__(self):
         self.root = None
-    
+
+# Working search method (iterative) (by me)
     def find(self, key):
         current = self.root
 
         while current:
             if current.key == key:
                 return current.value
-            elif current.key > key:
-                current = current.right
             elif current.key < key:
+                current = current.right
+            elif current.key > key:
                 current = current.left
 
-        if current:
-            return current.value
-        else:
-            return None
+        return None
+
+# Working insertion method (iterative) (by me)
+    def add(self, key, value = None):
+        current = self.root
+        new_node = TreeNode(key, value)
+        
+        if current == None:
+            self.root = new_node
+            return
+        
+        while current:
+            if key >= current.key:
+                if not current.right:
+                    current.right = new_node
+                    return
+                current = current.right
+            elif key < current.key:
+                if not current.left:
+                    current.left = new_node
+                    return
+                current = current.left
+
+# Working insertion method (recursive)
+    def add_recursively_helper(self, current, new_node):
+            # Base case: root is none
+            if not current:
+                self.root = new_node
+                return
+
+            # Recursive case: not a leaf
+            if new_node.key >= current.key:
+                if not current.right: # Base case: leaf found
+                    current.right = new_node
+                    return
+                else:
+                    self.add_recursively_helper(current.right, new_node)
+            if new_node.key < current.key:
+                if not current.left:
+                    current.left = new_node
+                    return
+                else:
+                    self.add_recursively_helper(current.left, new_node)
+
+    def add_recursively(self, key, value = None):
+            new_node = TreeNode(key, value)
+            self.add_recursively_helper(self.root, new_node)
+            return
